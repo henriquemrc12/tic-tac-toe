3822dbb69caafbcd9d561cf3df632cb18a37032c
henriquemrc12 <henriquemarcel2244@gmail.com>
2022-07-25 10:10:58 -0300

    :star: Project created

---
+++ src/main/java/TicTacToe.java
@@ -0,0 +1,59 @@
+import java.io.IOException;
+
+public class TicTacToe {
+
+    private static final int COLUMNS = 3;
+    private static final int ROWS = 3;
+
+    private static String[][] table = {
+            {"1", "2", "3"},
+            {"4", "5", "6"},
+            {"7", "8", "9"}
+    };
+
+    public static void main(String[] args) {
+        startGame();
+    }
+
+    private static void startGame() {
+        boolean isExit = false;
+
+        printTitle();
+        printTable();
+
+        while (!isExit) {
+            readPlay();
+            pcPlay();
+            isExit = checkGameStatus();
+            printTable();
+        }
+    }
+
+    private static void printTitle() {
+        System.out.println("|--------------------TIC TAC TOE--------------------|");
+    }
+
+    private static void printTable() {
+        for (int i = 0; i < COLUMNS; i++) {
+            for (int j = 0; j < ROWS; j++) {
+                String endRow = ((j + 1) < ROWS) ? " |" : " ";
+                System.out.print(" " + table[i][j] + endRow);
+            }
+            if ((i + 1) < COLUMNS)
+                System.out.println("\n----------");
+        }
+    }
+
+    private static boolean checkGameStatus() {
+        return true;
+    }
+
+    private static void pcPlay() {
+        return;
+    }
+
+    private static void readPlay() {
+
+        return;
+    }
+}
